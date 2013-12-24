7f53577e8d497da1e5f20eb328e491f812c66541
Ralf Westphal <info@ralfw.de>
2013-12-24 15:19:18 +0100

    Kata FizzBuzz

---
+++ kata fizzbuzz/fizzbuzz/Program.fs
@@ -0,0 +1,14 @@
+﻿let fizzbuzz max =
+  let convert n =
+    let isFizz n = n % 3 = 0
+    let isBuzz n = n % 5 = 0
+    let isFizzBuzz n = isFizz n && isBuzz n
+  
+    if   isFizzBuzz n then "FizzBuzz"
+    elif isFizz n     then "Fizz"
+    elif isBuzz n     then "Buzz"
+    else                   n.ToString()
+    
+  [for i in 1..max -> convert i]
+
+printfn "%A" (fizzbuzz 20)
