a8adfd9eec3eca778ed0129b5d2c0e40a99cdca6
Ralf Westphal <info@ralfw.de>
2013-12-24 23:43:11 +0100

    Kata Tannenbaum

---
+++ kata tannenbaum/tannenbaum/Program.fs
@@ -0,0 +1,22 @@
+﻿let tannenbaum baumhöhe =
+  let einrücken n = String.replicate n " "
+  let stamm baumhöhe = 
+    einrücken (baumhöhe-1)
+    + "I"
+
+  let rec geäst baumhöhe astnummer =
+    let ast baumhöhe astnummer = 
+      einrücken (baumhöhe - astnummer)
+      + String.replicate (2 * astnummer - 1) "X"
+      + "\n"
+      
+    if astnummer = 1 then
+      ast baumhöhe astnummer
+    else
+      geäst baumhöhe (astnummer-1) 
+      + ast baumhöhe astnummer
+      
+  geäst baumhöhe baumhöhe
+  + stamm baumhöhe
+
+printfn "%s" (tannenbaum 5)
