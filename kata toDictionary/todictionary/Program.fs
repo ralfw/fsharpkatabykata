6e4505ba22c07216cf1a6a165c617d707904ca1a
Ralf Westphal <info@ralfw.de>
2013-12-25 12:28:42 +0100

    kata ToDictionary

---
+++ kata toDictionary/todictionary/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+
+let toDictionary konfig =
+  let in_zuweisungen_zerlegen (konfig:string) =
+    konfig.Split ([|';'|], StringSplitOptions.RemoveEmptyEntries)
+  
+  let in_name_wert_paare_zerlegen (zuweisungen:string array) =
+    let zerlegen (zuweisung:string) = 
+      let name_wert = zuweisung.Split '='
+      (name_wert.[0], name_wert.[1])
+    zuweisungen |> Array.map zerlegen
+
+  konfig |> in_zuweisungen_zerlegen 
+         |> in_name_wert_paare_zerlegen 
+         |> Map.ofArray
+  
+printfn "%A" (toDictionary "a=1;bc=2;;a=42")
