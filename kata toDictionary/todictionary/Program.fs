f518fd8e72adf1aadcad4092f9ceb4a7a9edd7b8
Ralf Westphal <info@ralfw.de>
2013-12-26 20:54:01 +0100

    Zuweisungszerlegung auf Pipeline-Op umgestellt

--- kata toDictionary/todictionary/Program.fs
+++ kata toDictionary/todictionary/Program.fs
@@ -2,13 +2,14 @@
 
 let toDictionary konfig =
   let in_zuweisungen_zerlegen (konfig:string) =
-    konfig.Split ([|';'|], StringSplitOptions.RemoveEmptyEntries)
+    konfig.Split ([|';'|], 
+                  StringSplitOptions.RemoveEmptyEntries)
   
   let in_name_wert_paare_zerlegen (zuweisungen:string array) =
-    let zerlegen (zuweisung:string) = 
-      let name_wert = zuweisung.Split '='
-      (name_wert.[0], name_wert.[1])
+    let zerlegen (z:string) = z.Split '='
+    let in_tupel_wandeln (a:string array) = (a.[0], a.[1])
     zuweisungen |> Array.map zerlegen
+                |> Array.map in_tupel_wandeln
 
   konfig |> in_zuweisungen_zerlegen 
          |> in_name_wert_paare_zerlegen 
