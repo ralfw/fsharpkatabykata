93880a64a1520314272a74e74dfb2b72add162d9
Ralf Westphal <info@ralfw.de>
2014-01-18 11:12:34 +0100

    Tests für ToDictionary mit NUnit

---
+++ Tests automatisieren/todictionary/test_todictionary.fs
@@ -0,0 +1,22 @@
+﻿module test_todictionary
+
+open NUnit.Framework
+
+[<TestFixture>]
+type todictionary() =
+    let mehrere_Zuweisungsfälle = 
+        [
+            ("a=1;b=2", [("a", "1"); ("b","2")]);
+            ("a=1;;c=3", [("a", "1"); ("c","3")]);
+            ("a=;b=2", [("a", ""); ("b","2")]);
+        ]
+
+    [<Test>] 
+    member x.eine_Zuweisung() =
+        let expected = Map.ofList [("a", "1")]
+        Assert.AreEqual(expected, ToDictionary.convert "a=1")
+
+    [<Test>]
+    member x.mehrere_Zuweisungen() =
+        let check (config, expected) = Assert.AreEqual(Map.ofList expected, ToDictionary.convert config)
+        mehrere_Zuweisungsfälle |> List.iter check
