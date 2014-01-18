0f0680b904363a711b5bce35792df7b96619f17b
Ralf Westphal <info@ralfw.de>
2014-01-18 10:40:14 +0100

    Umformatierungen

--- kata zustandsautomat/zustandsautomat/fließkommazahlen.fs
+++ kata zustandsautomat/zustandsautomat/fließkommazahlen.fs
@@ -7,27 +7,27 @@ type Geprüft =
 // ["-"] d {d} ["." d {d}] 
 let private d = "0123456789"
 let private zustandsgraph = Map [
-                                    (0, [("-", 1); (d, 2)]);
-                                    (1, [(d, 2)]);
-                                    (2, [(d, 2); ("†", 99); (",", 3)]);
-                                    (3, [(d, 4)]);
-                                    (4, [(d, 4); ("†", 99)]);
-                                    (99, [])
-                                ]
+    (0, [("-", 1); (d, 2)]);
+    (1, [(d, 2)]);
+    (2, [(d, 2); ("†", 99); (",", 3)]);
+    (3, [(d, 4)]);
+    (4, [(d, 4); ("†", 99)]);
+    (99, [])
+]
 
 let private ist_passender_übergang (zeichen:char) (erlaubteZeichen:string, _)  =
-  erlaubteZeichen.IndexOf(zeichen) >= 0
+    erlaubteZeichen.IndexOf(zeichen) >= 0
   
 let private übergang_finden zeichen übergänge =
-  übergänge |> List.find (ist_passender_übergang zeichen)
+    übergänge |> List.find (ist_passender_übergang zeichen)
 
 let private übergänge_für_zustand_finden zustand =
   zustandsgraph |> Map.find zustand
 
 let private zustand_wechseln zeichen zustand =
-  let übergänge = übergänge_für_zustand_finden zustand
-  let (_, neuerZustand) = übergang_finden zeichen übergänge
-  neuerZustand
+    let übergänge = übergänge_für_zustand_finden zustand
+    let (_, neuerZustand) = übergang_finden zeichen übergänge
+    neuerZustand
   
 let prüfen (zeichenkette:string) =
   let rec prüfen' zustand zeichenkette =
